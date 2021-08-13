d2b6a972533e483a156a14a01301eaaa7b440dad
bitcamp <bitcamp@DESKTOP-SDLOUKF>
2021-08-13 16:34:58 +0900

    0813 commit

---
+++ src/object/ClassExample.java
@@ -0,0 +1,15 @@
+package object;
+
+public class ClassExample {
+	public static void main(String[] args) {
+//		Class clazz =Car.class;
+//		Class clazz=Class.forName("gg");
+//		Car car =new Car();
+//		Class clazz =car.getClass();
+		
+//		System.out.println(clazz.getName());
+//		System.out.println(clazz.getSimpleName());
+//		System.out.println(clazz.getPackage().getName());
+	}
+
+}
---
+++ src/object/StringToPrimitiveValueExample.java
@@ -0,0 +1,15 @@
+package object;
+
+public class StringToPrimitiveValueExample {
+	public static void main(String[] args) {
+		int v1 = Integer.parseInt("10");
+		double v2 = Double.parseDouble("3.14");
+		boolean v3 =Boolean.parseBoolean("true");
+		
+		System.out.println("v1 : "+v1);
+		System.out.println("v2 : "+v2);
+		System.out.println("v3 : "+v3);
+		
+	}
+
+}
